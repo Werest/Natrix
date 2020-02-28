e483315bfe2ca0cf326f97a14a2e3bc3c0ee5dbd
werest <weresttrade@ya.ru>
2020-02-28 17:06:51 +0300

    Тернарный оператор

--- practice.php
+++ practice.php
@@ -6,6 +6,15 @@ echo '<div class="container">';
 require_once 'views/view_practice.php';
 $g = new George();
 echo $g->ha();
+echo "<br>";
+
+$cl = clone $g;
+echo $cl->ha(44, 55);
+echo "<br>";
+
+$x = 10;
+$x = $x < 0 ? 'Yes' : 'No';
+echo $x;
 
 
 echo '</div>';
